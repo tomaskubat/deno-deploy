4f776fc637eebf76ecb15a403554289ba1b9d9a9
Tomas Kubat <tomas.kubat@gmail.com>
2021-06-25 23:54:43 +0200

    comment-in dotenv

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { Application } from "https://deno.land/x/oak@v7.5.0/mod.ts";
-import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
+//import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 import { router } from "./router.ts"
 import { MiddlewareContext } from "./types.ts"
 
-config({ export: true });
+//config({ export: true });
 
 const app = new Application<MiddlewareContext>();
 app.use(router.routes());
